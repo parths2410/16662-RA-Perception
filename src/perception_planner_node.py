854dc799a8ff7cd37ec4b9f55711d917e79dc1ad
Parth Shah <pnshah@andrew.cmu.edu>
2024-03-17 20:43:42 -0400

    merged to one file

--- src/perception_planner_node.py
+++ src/perception_planner_node.py
@@ -7,9 +7,48 @@ from sensor_msgs.msg import PointCloud2
 from sensor_msgs.point_cloud2 import read_points_list, create_cloud
 from sensor_msgs.msg import PointField
 from std_msgs.msg import Header
+from nav_msgs.msg import Path, PoseStamped
 
 import numpy as np
 
+class ShapeGenerator:
+    def __init__(self, c_x, c_y, step_size=2e-2):
+        self.c_x = c_x
+        self.c_y = c_y
+        self.step_size = step_size
+    
+    def generate_rectangle_points(self, l, b):
+        # Calculate corner points
+        top_right = (l/2 + self.c_x, b/2 + self.c_y)
+        top_left = (-l/2 + self.c_x, b/2 + self.c_y)
+        bottom_left = (-l/2 + self.c_x, -b/2 + self.c_y)
+        bottom_right = (l/2 + self.c_x, -b/2 + self.c_y)
+        
+        # Generate points along each edge, separated by 2 mm
+        points = []
+
+        # Top edge
+        points += [(x, top_right[1]) for x in np.arange(top_right[0], top_left[0], -self.step_size)]
+        # Left edge
+        points += [(top_left[0], y) for y in np.arange(top_left[1], bottom_left[1], -self.step_size)]
+        # Bottom edge
+        points += [(x, bottom_left[1]) for x in np.arange(bottom_left[0], bottom_right[0], self.step_size)]
+        # Right edge
+        points += [(bottom_right[0], y) for y in np.arange(bottom_right[1], top_right[1], self.step_size)]
+        
+        return points
+    
+    def generate_circle_points(self, r):
+        # Determine the number of points needed, approximating circumference as 2*pi*r
+        circumference = 2 * np.pi * r
+        num_points = int(circumference / self.step_size)
+        
+        # Generate points along the circumference
+        theta = np.linspace(0, 2 * np.pi, num_points, endpoint=False)
+        points = [(r * np.cos(t) + self.c_x, r * np.sin(t) + self.c_y) for t in theta]
+        
+        return points
+        
 class PerceptionPlanner:
     def __init__(self):
         rospy.init_node('perception_planner')
@@ -25,6 +64,7 @@ class PerceptionPlanner:
         rospy.loginfo('Service %s is ready', service_name)
 
         object_center = (0, -0.17)
+        self.shape_gen = ShapeGenerator(*object_center, step_size=2e-2)
 
         rospy.spin()
     
@@ -45,15 +85,19 @@ class PerceptionPlanner:
 
         print("Filtering")
         pcd_np_filtered = self.filter_pcd(pcd_np, (-0.2, 0.2), (-0.4, 0))
+        self.pcd = pcd_np_filtered
         print("PCD Filtered Shape - ", pcd_np_filtered.shape)
         
         print("Publishing filtered")
         filtered_msg = self.generate_pc2_msg(pcd_np_filtered)
         self.filtered_pub.publish(filtered_msg)
         
-        path_2d = 
+        path_2d = self.get_shape_points(shape)
+        path_3d = self.project_to_pcd(path_2d)
 
-        return PerceivePlanResponse(True)
+        path_msg = self.generate_path_message(path_3d)
+
+        return PerceivePlanResponse(path_msg)
     
     def filter_pcd(self, pcd, x_range, y_range, z_range=None):
         x_mask = (pcd[:, 0] >= x_range[0]) & (pcd[:, 0] <= x_range[1])
@@ -73,20 +117,51 @@ class PerceptionPlanner:
         pcd_arr = pcd_arr[:, :3]
 
         pcd_arr_filtered = self.filter_pcd(pcd_arr, (-0.2, 0.2), (-0.4, 0))
-
+ 
         filtered_msg = self.generate_pc2_msg(pcd_arr_filtered)
         self.pcd_pub.publish(filtered_msg)
 
-
-
     def get_shape_points(self, shape):
-        pass
+        if shape == "rectangle":
+            l, b = 0.2, 0.3
+            points = self.shape_gen.generate_rectangle_points(l, b)
+        elif shape == "circle":
+            r = 0.2
+            points = self.shape_gen.generate_circle_points(r)
+        elif shape == "square":
+            l, b = 0.2, 0.2
+            points = self.shape_gen.generate_rectangle_points(l, b)
+
+        return points
 
     def find_depth(self, x, y) :
-        pass
+        thresh = 0.001  # 1mm
+        pcd = self.pcd
+
+        mask = (pcd[:, 0] >= x - thresh) & (pcd[:, 0] <= x + thresh) & (pcd[:, 1] >= y - thresh) & (pcd[:, 1] <= y + thresh)
+        depth = pcd[mask][:, 2]
+        return np.mean(depth)
+
+    def project_to_pcd(self, path_2d):
+        path_3d = []
+        for x, y in path_2d:
+            depth = self.find_depth(x, y)
+            path_3d.append((x, y, depth))
+        return np.array(path_3d)
 
     def generate_path_message(self, path_points):
-        pass
+        path = Path()
+        path.header.frame_id = self.frame_id
+        path.header.stamp = rospy.Time.now()
+        for x, y, z in path_points:
+            pose = PoseStamped()
+            pose.pose.position.x = x
+            pose.pose.position.y = y
+            pose.pose.position.z = z
+            path.poses.append(pose)
+
+        return path
+
 
     def generate_pc2_msg(self, pcd_arr):
         fields = [PointField('x', 0, PointField.FLOAT32, 1),
